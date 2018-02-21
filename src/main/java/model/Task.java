aa649c13cd2cb07e89d6bcd87846eba8f81f4af8
commonmeen <commonmeen@gmail.com>
2018-02-21 21:30:24 +0700

    [Add] addTask and getTaskByTaskNo on Task model

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -25,7 +25,8 @@ public class Task {
     public Task() {
     }
 
-    public Task(String task_Name, String deadline, int score, String description, int c_No, int r_No) {
+    public Task(int task_No, String task_Name, String deadline, int score, String description, int c_No, int r_No) {
+        this.task_No = task_No;
         this.task_Name = task_Name;
         this.deadline = deadline;
         this.score = score;
@@ -91,6 +92,12 @@ public class Task {
     public void setR_No(int r_No) {
         this.r_No = r_No;
     }
+
+    @Override
+    public String toString() {
+        return "Task{" + "task_No=" + task_No + ", task_Name=" + task_Name + ", deadline=" + deadline + ", score=" + score + ", description=" + description + ", c_No=" + c_No + ", r_No=" + r_No + '}';
+    }
+    
     
     
 
@@ -120,10 +127,10 @@ public class Task {
         
     }
     
-    public void getTask(int task_no){
+    public Task getTaskByTaskNo(int task_no){
         Connection con = null;
         String SQL = "Select * from Task where task_no = ?";
-        Task task = new Task();
+        Task task = null;
         try {
             con = ConnectionBuilder.getConnection();
             PreparedStatement ps = con.prepareStatement(SQL);
@@ -131,13 +138,14 @@ public class Task {
             ResultSet rs = ps.executeQuery();
             
             while(rs.next()){
-                this.task_Name = rs.getString("Task_Name");
-                this.deadline = rs.getString("Deadline");
-                this.score = rs.getInt("Score");
-                this.description = rs.getString("Description");
-                this.c_No = rs.getInt("C_No");
-                this.r_No = rs.getInt("R_No");
-                       
+                task = new Task(
+                        rs.getInt("task_No"),
+                        rs.getString("task_Name"), 
+                        rs.getString("deadline"), 
+                        rs.getInt("score"), 
+                        rs.getString("description"), 
+                        rs.getInt("c_No"), 
+                        rs.getInt("r_No"));
             }
             System.out.println(this.description);
                 
@@ -145,14 +153,15 @@ public class Task {
             System.out.println(e);
         }
         
-        return ;
+        return task;
              
     }
     
     public static void main(String[] args) {
-        Task t1 = new Task("test", "20/2/2018", 20, "ทดลองปลูกถั่ว", 200003,400001);
-        t1.addTask(t1);
-        t1.getTask(100015);
+        //Task t1 = new Task("test", "20/2/2018", 20, "ทดลองเลี้ยงปลา", 200003,400001);
+        //t1.addTask(t1);
+        Task t2 = new Task().getTaskByTaskNo(100026);
+        System.out.println(t2);
         
         
         
